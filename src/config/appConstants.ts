667e33dd39c8213c268dcc9b9d7d42ccae29fd1d
Edison Huang <107197406+edisontw@users.noreply.github.com>
2026-07-05 15:23:45 +0800

    Add app constants

---
+++ src/config/appConstants.ts
@@ -0,0 +1,4 @@
+export const LIGHT_API_BASE = "https://light.pepepow.net";
+export const DEFAULT_FEE = 0.001;
+export const REFRESH_COOLDOWN_MS = 15000;
+export const POST_SUBMIT_REFRESH_DELAYS_MS = [12000, 45000];
