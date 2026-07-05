89aa62bb84e6e21f56a2e70206299f7dcc96be9a
Edison Huang <107197406+edisontw@users.noreply.github.com>
2026-07-05 22:38:09 +0800

    Render PEPEW logo badge component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import AppPhase5Shell from "./AppPhase5Shell";
+import { PepewLogoBadge } from "./components/PepewLogoBadge";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <PepewLogoBadge />
     <AppPhase5Shell />
   </StrictMode>,
 );
