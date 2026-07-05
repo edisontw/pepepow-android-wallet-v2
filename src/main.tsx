76425d946d1d94d1878c21acbc9aba16e3c956d5
Edison Huang <107197406+edisontw@users.noreply.github.com>
2026-07-05 23:25:56 +0800

    Remove floating security shell from app entry

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import AppPhase5Shell from "./AppPhase5Shell";
-import { PepewLogoBadge } from "./components/PepewLogoBadge";
+import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <PepewLogoBadge />
-    <AppPhase5Shell />
+    <App />
   </StrictMode>,
 );
