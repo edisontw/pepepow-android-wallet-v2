5a5e29b72056b73f28f37b709d8d4e3bde91bcec
Edison Huang <107197406+edisontw@users.noreply.github.com>
2026-07-05 08:30:21 +0800

    fix: add compose theme placeholder

---
+++ app/src/main/java/net/pepepow/wallet/ui/theme/Theme.kt
@@ -0,0 +1,9 @@
+package net.pepepow.wallet.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PepepowWalletTheme(content: @Composable () -> Unit) {
+    MaterialTheme(content = content)
+}
