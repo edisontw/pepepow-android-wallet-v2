6e6e524c8e4babe033b5ee6916da5cf9d3f422a5
Edison Huang <107197406+edisontw@users.noreply.github.com>
2026-07-05 11:33:07 +0800

    Add PEPEPOW network parameters for phase3 branch

---
+++ src/wallet/pepepowParams.ts
@@ -0,0 +1,8 @@
+export const PEPEPOW_PARAMS = {
+  p2pkhVersion: 55,
+  p2shVersion: 16,
+  wifVersion: 204,
+  coinType: 5,
+  lightApiBase: "https://light.pepepow.net",
+  reference: "edisontw/pepepow-android-wallet external/dashj/core/src/main/java/org/bitcoinj/params/MainNetParams.java"
+} as const;
